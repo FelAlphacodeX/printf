3c50b2b637b84b9755c8944cef09fce6c9c3ab37
AbigailOpoku101 <acheampomaaoabbyj@gmail.com>
2022-11-22 16:19:24 -0800

    group

--- _printf.c
+++ _printf.c
@@ -29,7 +29,8 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	int i, wid, prec, ret = 0;
 	char tmp;
 	unsigned char flags, len;
-	unsigned int (*f)(va_list, buffer_t *, unsigned char, int, int, unsigned int);
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
 	for (i = 0; *(format + i); i++)
 	{
